465c7067b17f11b4d372a48f9257fd0d4223c375
dachendev <13964975+dachendev@users.noreply.github.com>
2024-09-04 14:28:26 -0500

    exercise 1.8

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -4,19 +4,33 @@ const Button = ({ handleClick, text }) => {
   return <button onClick={handleClick}>{text}</button>
 }
 
+const Statistics = ({ good, neutral, bad }) => {
+  const goodWeight = 1;
+  const neutralWeight = 0;
+  const badWeight = -1;
+
+  const getAll = () => good + neutral + bad;
+  const getWeightedAvg = () => (good * goodWeight + neutral * neutralWeight + bad * badWeight) / getAll();
+  const getPositive = () => (good / getAll()) * 100;
+
+  return (
+    <div>
+      <h1>statistics</h1>
+      <p>good {good}</p>
+      <p>neutral {neutral}</p>
+      <p>bad {bad}</p>
+      <p>all {getAll()}</p>
+      <p>average {getWeightedAvg()}</p>
+      <p>positive {getPositive()} %</p>
+    </div>
+  )
+}
+
 const App = () => {
   // save clicks of each button to its own state
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
-
-  const goodWeight = 1;
-  const neutralWeight = 0;
-  const badWeight = -1;
-
-  const getCount = () => good + neutral + bad;
-  const getAverage = () => (good * goodWeight + neutral * neutralWeight + bad * badWeight) / getCount();
-  const getPositive = () => (good / getCount()) * 100;
 
   return (
     <div>
@@ -26,15 +40,8 @@ const App = () => {
         <Button text="neutral" handleClick={() => setNeutral(neutral + 1)} />
         <Button text="bad" handleClick={() => setBad(bad + 1)} />
       </div>
-      <h1>statistics</h1>
-      <div>
-        <p>good {good}</p>
-        <p>neutral {neutral}</p>
-        <p>bad {bad}</p>
-        <p>all {getCount()}</p>
-        <p>average {getAverage()}</p>
-        <p>positive {getPositive()} %</p>
-      </div>
+
+      <Statistics good={good} neutral={neutral} bad={bad} />
     </div>
   )
 }
