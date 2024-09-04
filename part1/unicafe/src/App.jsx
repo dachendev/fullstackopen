5299cf8585879a583d2878d6d25d06553716337d
dachendev <13964975+dachendev@users.noreply.github.com>
2024-09-04 14:25:37 -0500

    exercise 1.7

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -10,17 +10,31 @@ const App = () => {
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
 
+  const goodWeight = 1;
+  const neutralWeight = 0;
+  const badWeight = -1;
+
+  const getCount = () => good + neutral + bad;
+  const getAverage = () => (good * goodWeight + neutral * neutralWeight + bad * badWeight) / getCount();
+  const getPositive = () => (good / getCount()) * 100;
+
   return (
     <div>
       <h1>give feedback</h1>
-      <Button text="good" handleClick={() => setGood(good + 1)} />
-      <Button text="neutral" handleClick={() => setNeutral(neutral + 1)} />
-      <Button text="bad" handleClick={() => setBad(bad + 1)} />
-
+      <div>
+        <Button text="good" handleClick={() => setGood(good + 1)} />
+        <Button text="neutral" handleClick={() => setNeutral(neutral + 1)} />
+        <Button text="bad" handleClick={() => setBad(bad + 1)} />
+      </div>
       <h1>statistics</h1>
-      <p>good {good}</p>
-      <p>neutral {neutral}</p>
-      <p>bad {bad}</p>
+      <div>
+        <p>good {good}</p>
+        <p>neutral {neutral}</p>
+        <p>bad {bad}</p>
+        <p>all {getCount()}</p>
+        <p>average {getAverage()}</p>
+        <p>positive {getPositive()} %</p>
+      </div>
     </div>
   )
 }
