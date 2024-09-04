fa51088f5524cd82243b582eb2f37323ee0681d2
dachendev <13964975+dachendev@users.noreply.github.com>
2024-09-04 15:02:38 -0500

    exercise 1.14

--- part1/anecdotes/src/App.jsx
+++ part1/anecdotes/src/App.jsx
@@ -2,20 +2,8 @@ import { useState } from 'react'
 
 const Button = ({ text, handleClick }) => <button onClick={handleClick}>{text}</button>;
 
-const App = () => {
-  const anecdotes = [
-    'If it hurts, do it more often.',
-    'Adding manpower to a late software project makes it later!',
-    'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
-    'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
-    'Premature optimization is the root of all evil.',
-    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
-    'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.',
-    'The only way to go fast, is to go well.'
-  ]
-
+const Daily = ({ anecdotes, points, setPoints }) => {
   const [selected, setSelected] = useState(0)
-  const [points, setPoints] = useState(Array.from({ length: anecdotes.length }).fill(0))
 
   const random = (min, max) => Math.floor(Math.random() * (max - min + 1)) + min;
 
@@ -32,13 +20,46 @@ const App = () => {
 
   return (
     <div>
+      <h1>Anecdote of the day</h1>
       <p>{anecdotes[selected]}</p>
       <p>has {points[selected]} votes</p>
-      <div>
-        <Button text="vote" handleClick={() => incrementPoints()} />
-        <Button text="next anecdote" handleClick={() => nextAnecdote()} />
-      </div>
+      <Button text="vote" handleClick={() => incrementPoints()} />
+      <Button text="next anecdote" handleClick={() => nextAnecdote()} />
     </div>
+  )
+}
+
+const MostVotes = ({ anecdotes, points }) => {
+  const getMaxIndex = () => points.reduce((maxIndex, currValue, currIndex, arr) => (currValue > arr[maxIndex] ? currIndex : maxIndex), 0);
+
+  return (
+    <div>
+      <h1>Anecdote with most votes</h1>
+      <p>{anecdotes[getMaxIndex()]}</p>
+      <p>has {points[getMaxIndex()]} votes</p>
+    </div>
+  )
+}
+
+const App = () => {
+  const anecdotes = [
+    'If it hurts, do it more often.',
+    'Adding manpower to a late software project makes it later!',
+    'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+    'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+    'Premature optimization is the root of all evil.',
+    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
+    'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.',
+    'The only way to go fast, is to go well.'
+  ]
+
+  const [points, setPoints] = useState(Array.from({ length: anecdotes.length }).fill(0))
+
+  return (
+    <>
+      <Daily anecdotes={anecdotes} points={points} setPoints={setPoints} />
+      <MostVotes anecdotes={anecdotes} points={points} />
+    </>
   )
 }
 
