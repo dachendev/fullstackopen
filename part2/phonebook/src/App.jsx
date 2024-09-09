3f2b2b59a31de9399d454a012f6da6c27211e929
dachendev <13964975+dachendev@users.noreply.github.com>
2024-09-09 12:14:55 -0400

    exercise 2.13

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,5 +1,6 @@
 import { useState, useEffect } from 'react'
 import axios from "axios";
+import personService from "./services/persons.js";
 
 const SearchFilter = ({ filter, setFilter }) => {
   return (
@@ -46,11 +47,8 @@ const App = () => {
   const [newNumber, setNewNumber] = useState("");
 
   useEffect(() => {
-    axios
-      .get("http://localhost:3001/persons")
-      .then(response => {
-        setPersons(response.data)
-      })
+    personService.getAll()
+      .then((returnedPersons) => setPersons(returnedPersons));
   }, [])
 
   const addPerson = (event) => {
@@ -60,9 +58,8 @@ const App = () => {
       number: newNumber,
     };
 
-    axios.post("http://localhost:3001/persons", personObject)
-      .then(response => {
-        const returnedPerson = response.data;
+    personService.create(personObject)
+      .then((returnedPerson) => {
         setPersons(persons.concat(returnedPerson));
         setNewName("");
         setNewNumber("");
