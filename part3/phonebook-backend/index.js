49453848c6cfbd29621462f44a1abb1ce67c11a9
dachendev <13964975+dachendev@users.noreply.github.com>
2024-09-11 18:07:40 -0400

    exercise 3.7

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -1,7 +1,11 @@
 const express = require("express");
+const morgan = require("morgan");
+
 const app = express();
 
+// middleware
 app.use(express.json());
+app.use(morgan("tiny"));
 
 const random = (min, max) => {
   return Math.floor(Math.random() * (max - min)) + max;
