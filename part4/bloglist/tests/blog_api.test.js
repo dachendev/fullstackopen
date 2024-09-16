95371f56ae5bc2f79a21341e03fde3d9bd3cc6e1
dachendev <13964975+dachendev@users.noreply.github.com>
2024-09-16 15:58:29 -0400

    exercise 4.8

---
+++ part4/bloglist/tests/blog_api.test.js
@@ -0,0 +1,34 @@
+const { test, after, beforeEach } = require('node:test')
+const assert = require('node:assert')
+const mongoose = require('mongoose')
+const supertest = require('supertest')
+const helper = require('./test_helper.js')
+const app = require('../app.js')
+const Blog = require('../models/blog.js')
+
+const api = supertest(app)
+
+beforeEach(async () => {
+  await Blog.deleteMany({})
+
+  const blogObjects = helper.initialBlogs.map(blog => new Blog(blog))
+  const promiseArray = blogObjects.map(blog => blog.save())
+  await Promise.all(promiseArray)
+})
+
+test('blogs are returned as json', async () => {
+  await api
+    .get('/api/blogs')
+    .expect(200)
+    .expect('Content-Type', /application\/json/)
+})
+
+test('all blogs are returned', async () => {
+  const res = await api.get('/api/blogs')
+  
+  assert.strictEqual(res.body.length, helper.initialBlogs.length)
+})
+
+after(async () => {
+  await mongoose.connection.close()
+})
