2aa8efe0ceffd55615d3fb1993e7d29b160eca40
dachendev <13964975+dachendev@users.noreply.github.com>
2024-09-16 16:40:35 -0400

    Refactoring tests

--- part4/bloglist/tests/blog_api.test.js
+++ part4/bloglist/tests/blog_api.test.js
@@ -16,77 +16,79 @@ beforeEach(async () => {
   await Promise.all(promiseArray)
 })
 
-test('blogs are returned as json', async () => {
-  await api
-    .get('/api/blogs')
-    .expect(200)
-    .expect('Content-Type', /application\/json/)
-})
-
-test('all blogs are returned', async () => {
-  const res = await api.get('/api/blogs')
-  
-  assert.strictEqual(res.body.length, helper.initialBlogs.length)
-})
-
-test('unique identifier property is named id', async () => {
-  const response = await api.get('/api/blogs')
-  const firstBlog = response.body[0]
-
-  assert(firstBlog.hasOwnProperty('id'))
-})
-
-describe('creating new blogs', () => {
-  test('creates a new blog', async () => {
-    const newBlog = {
-      title: 'Type wars',
-      author: 'Robert C. Martin',
-      url: 'http://blog.cleancoder.com/uncle-bob/2016/05/01/TypeWars.html',
-      likes: 2
-    }
-  
+describe('when there is initially some blogs saved', () => {
+  test('blogs are returned as json', async () => {
     await api
-      .post('/api/blogs')
-      .send(newBlog)
-      .expect(201)
+      .get('/api/blogs')
+      .expect(200)
       .expect('Content-Type', /application\/json/)
-  
-    const blogsAfter = await helper.blogsInDb()
-    assert.strictEqual(blogsAfter.length, helper.initialBlogs.length + 1)
-  
-    const titles = blogsAfter.map(b => b.title)
-    assert(titles.includes(newBlog.title))
   })
 
-  test('missing likes defaults to zero', async () => {
-    const newBlog = {
-      title: 'Type wars',
-      author: 'Robert C. Martin',
-      url: 'http://blog.cleancoder.com/uncle-bob/2016/05/01/TypeWars.html'
-    }
-  
-    const response = await api.post('/api/blogs').send(newBlog)
-  
-    const createdBlog = (await helper.blogsInDb()).find(b => b.id === response.body.id)
-    assert.strictEqual(createdBlog.likes, 0)
+  test('all blogs are returned', async () => {
+    const res = await api.get('/api/blogs')
+
+    assert.strictEqual(res.body.length, helper.initialBlogs.length)
   })
 
-  test('blog with missing title is invalid', async () => {
-    const newBlog = {
-      author: 'Robert C. Martin',
-      url: 'http://blog.cleancoder.com/uncle-bob/2016/05/01/TypeWars.html'
-    }
+  test('unique identifier property is named id', async () => {
+    const response = await api.get('/api/blogs')
+    const firstBlog = response.body[0]
 
-    await api.post('/api/blogs').send(newBlog).expect(400)
+    assert(firstBlog.hasOwnProperty('id'))
   })
 
-  test('blog with missing url is invalid', async () => {
-    const newBlog = {
-      title: 'Type wars',
-      author: 'Robert C. Martin',
-    }
+  describe('addition of a new blog', () => {
+    test('succeeds with valid data', async () => {
+      const newBlog = {
+        title: 'Type wars',
+        author: 'Robert C. Martin',
+        url: 'http://blog.cleancoder.com/uncle-bob/2016/05/01/TypeWars.html',
+        likes: 2
+      }
 
-    await api.post('/api/blogs').send(newBlog).expect(400)
+      await api
+        .post('/api/blogs')
+        .send(newBlog)
+        .expect(201)
+        .expect('Content-Type', /application\/json/)
+
+      const blogsAfter = await helper.blogsInDb()
+      assert.strictEqual(blogsAfter.length, helper.initialBlogs.length + 1)
+
+      const titles = blogsAfter.map(b => b.title)
+      assert(titles.includes(newBlog.title))
+    })
+
+    test('missing likes defaults to 0', async () => {
+      const newBlog = {
+        title: 'Type wars',
+        author: 'Robert C. Martin',
+        url: 'http://blog.cleancoder.com/uncle-bob/2016/05/01/TypeWars.html'
+      }
+
+      const response = await api.post('/api/blogs').send(newBlog)
+
+      const createdBlog = (await helper.blogsInDb()).find(b => b.id === response.body.id)
+      assert.strictEqual(createdBlog.likes, 0)
+    })
+
+    test('fails with status code 400 if title invalid', async () => {
+      const newBlog = {
+        author: 'Robert C. Martin',
+        url: 'http://blog.cleancoder.com/uncle-bob/2016/05/01/TypeWars.html'
+      }
+
+      await api.post('/api/blogs').send(newBlog).expect(400)
+    })
+
+    test('fails with status code 400 if url invalid', async () => {
+      const newBlog = {
+        title: 'Type wars',
+        author: 'Robert C. Martin',
+      }
+
+      await api.post('/api/blogs').send(newBlog).expect(400)
+    })
   })
 })
 
