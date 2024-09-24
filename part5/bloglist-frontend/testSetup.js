31eaa21082465e94e0fcbe22131dc6a51222299a
dachendev <13964975+dachendev@users.noreply.github.com>
2024-09-24 14:07:41 -0500

    Add vitest

---
+++ part5/bloglist-frontend/testSetup.js
@@ -0,0 +1,8 @@
+// testSetup.js
+import { afterEach } from 'vitest'
+import { cleanup } from '@testing-library/react'
+import '@testing-library/jest-dom/vitest'
+
+afterEach(() => {
+  cleanup()
+})
