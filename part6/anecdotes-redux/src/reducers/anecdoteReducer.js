8a6eb927661c4c2101e5f6214299c9d55f8baf0b
dachendev <13964975+dachendev@users.noreply.github.com>
2024-09-26 13:04:12 -0500

    exercise 6.3

--- part6/anecdotes-redux/src/reducers/anecdoteReducer.js
+++ part6/anecdotes-redux/src/reducers/anecdoteReducer.js
@@ -20,10 +20,23 @@ const asObject = (anecdote) => {
 const initialState = anecdotesAtStart.map(asObject)
 
 const reducer = (state = initialState, action) => {
-  console.log('state now: ', state)
+  // console.log('state now: ', state)
   console.log('action', action)
+
+  if (action.type === 'anecdote/add-vote') {
+    const nextState = [...state]
+    const anecdote = nextState.find(p => p.id === action.payload)
+    anecdote.votes++
+    
+    return nextState
+  }
 
   return state
 }
+
+export const addVoteTo = (id) => ({
+  type: 'anecdote/add-vote',
+  payload: id
+})
 
 export default reducer
