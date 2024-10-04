64da95b310dda0ca2d6fe9537afc7c45ded10b6b
dachendev <13964975+dachendev@users.noreply.github.com>
2024-10-04 15:40:17 -0400

    add graphql-tag and exercise 8.1

--- part8/library-backend/src/index.js
+++ part8/library-backend/src/index.js
@@ -1,5 +1,6 @@
 const { ApolloServer } = require("@apollo/server");
 const { startStandaloneServer } = require("@apollo/server/standalone");
+const gql = require("graphql-tag");
 
 let authors = [
   {
@@ -97,15 +98,19 @@ let books = [
   you can remove the placeholder query once your first one has been implemented 
 */
 
-const typeDefs = `
+const typeDefs = gql`
   type Query {
     dummy: Int
+    bookCount: Int!
+    authorCount: Int!
   }
 `;
 
 const resolvers = {
   Query: {
     dummy: () => 0,
+    bookCount: () => books.length,
+    authorCount: () => authors.length,
   },
 };
 
