0c07e67c5a647547d90ff197122935794caf3635
dachendev <13964975+dachendev@users.noreply.github.com>
2024-10-12 18:33:35 -0400

    exercise 8.19

--- part8/library/frontend/src/components/Books.jsx
+++ part8/library/frontend/src/components/Books.jsx
@@ -1,17 +1,33 @@
 import { useQuery } from '@apollo/client'
 import { ALL_BOOKS } from '../queries'
+import { useState } from 'react'
 
 const Books = () => {
   const { loading, error, data } = useQuery(ALL_BOOKS)
+  const [selectedGenre, setSelectedGenre] = useState('')
 
   if (loading) return <p>loading...</p>
   if (error) return <p>Error: {error.message}</p>
 
   const books = data.allBooks
+  const genres = [...new Set(books.flatMap((book) => book.genres))]
+  const booksByGenre = selectedGenre ? books.filter((book) => book.genres.includes(selectedGenre)) : books
 
   return (
     <div>
       <h2>books</h2>
+      <div>
+        in genre:
+        <select value={selectedGenre} onChange={(e) => setSelectedGenre(e.target.value)}>
+          <option value="">all genres</option>
+          {genres.map((genre) => (
+            <option key={genre}>{genre}</option>
+          ))}
+        </select>
+        <button type="button" onClick={() => setSelectedGenre('')}>
+          reset
+        </button>
+      </div>
       <table>
         <thead>
           <tr>
@@ -21,7 +37,7 @@ const Books = () => {
           </tr>
         </thead>
         <tbody>
-          {books.map((a) => (
+          {booksByGenre.map((a) => (
             <tr key={a.title}>
               <td>{a.title}</td>
               <td>{a.author.name}</td>
