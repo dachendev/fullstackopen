9439f91ccd72fe7dd2b1147465cff56dab225722
dachendev <13964975+dachendev@users.noreply.github.com>
2024-11-04 18:35:05 -0600

    exercise 12.22

--- part9/flight-diary/backend/src/index.ts
+++ part9/flight-diary/backend/src/index.ts
@@ -1,8 +1,11 @@
 import express from "express";
 import diaryRouter from "./routes/diaries";
 import cors from "cors";
+import process from "node:process";
+import http from "node:http";
 
 const app = express();
+const server = http.createServer(app);
 
 app.use(cors());
 app.use(express.json());
@@ -16,6 +19,25 @@ app.get("/ping", (_req, res) => {
 
 app.use("/api/diaries", diaryRouter);
 
-app.listen(PORT, () => {
+server.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
+
+const closeGracefully = (signal: NodeJS.Signals) => {
+  console.log(`Received ${signal}. Shutting down gracefully...`);
+
+  server.close(() => {
+    console.log("Server shut down.");
+    process.exit(0);
+  });
+
+  server.closeAllConnections();
+
+  setTimeout(() => {
+    console.log("Forcing shutdown due to timeout.");
+    process.kill(process.pid, signal);
+  }, 10000);
+};
+
+process.on("SIGINT", closeGracefully);
+process.on("SIGTERM", closeGracefully);
